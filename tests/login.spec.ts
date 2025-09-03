2d4884c140479609f24396623d0bc7812c0d7bb5
misha.chernikov <misha.chernikov86@gmail.com>
2025-09-03 19:58:12 +0300

    Refactor login tests to improve readability and maintainability

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,13 +2,22 @@ import { test, expect } from '@playwright/test';
 
 test.describe("User login to Demobank", () => {
 
-  test('successful login with correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('testerLO');
-    await page.getByTestId('password-input').fill('00000000');
+  test.only('successful login with correct credentials', async ({ page }) => {
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'testerLO';
+    const userPassword = '12345678';
+    const expectedUserName = "Jan Demobankowy";
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
     await page.getByTestId('user-name').click();
-    await expect(page.getByTestId('user-name')).toHaveText("Jan Demobankowy")
+    
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName)
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
