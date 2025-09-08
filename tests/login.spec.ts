57bf2e3e1eb050f432f407c891c35a9a75a6a6ce
misha.chernikov <misha.chernikov86@gmail.com>
2025-09-08 13:16:03 +0300

    Update README and tests for improved clarity and functionality; add Prettier configuration

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test.describe("User login to Demobank", () => {
-
+test.describe('User login to Demobank', () => {
   test('successful login with correct credentials', async ({ page }) => {
     // Arrange
     const url = 'https://demo-bank.vercel.app/';
     const userId = 'testerLO';
     const userPassword = '12345678';
-    const expectedUserName = "Jan Demobankowy";
+    const expectedUserName = 'Jan Demobankowy';
 
     // Act
     await page.goto(url);
@@ -15,9 +14,9 @@ test.describe("User login to Demobank", () => {
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
     await page.getByTestId('user-name').click();
-    
+
     // Assert
-    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName)
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
@@ -25,7 +24,9 @@ test.describe("User login to Demobank", () => {
     await page.getByTestId('login-input').fill('tester');
     await page.getByTestId('password-input').click();
     await page.getByTestId('error-login-id').click();
-    await expect(page.getByTestId('error-login-id')).toHaveText("identyfikator ma min. 8 znaków")
+    await expect(page.getByTestId('error-login-id')).toHaveText(
+      'identyfikator ma min. 8 znaków',
+    );
   });
 });
 
@@ -34,5 +35,7 @@ test('unsuccessful login with too short password', async ({ page }) => {
   await page.getByTestId('login-input').fill('testerLO');
   await page.getByTestId('password-input').fill('12345');
   await page.getByTestId('password-input').blur();
-  await expect(page.getByTestId('error-login-password')).toHaveText("hasło ma min. 8 znaków")
+  await expect(page.getByTestId('error-login-password')).toHaveText(
+    'hasło ma min. 8 znaków',
+  );
 });
